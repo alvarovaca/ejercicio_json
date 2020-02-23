7c923915cb1616c504b586c5d3ebe165b8322f58
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-02-23 09:50:20 +0100

    Corrección de bugs y progreso en el programa principal.

--- Funciones.py
+++ Funciones.py
@@ -28,12 +28,41 @@ def BalancePersonas(minimo,maximo,datos):
     apellidos=[]
     balances=[]
     for i in datos:
-        if i["balance"]>=minimo and i["balance"]<=maximo:
+        if float(i["balance"])>=minimo and float(i["balance"])<=maximo:
             nombres.append(i["name"]["first"])
             apellidos.append(i["name"]["last"])
             balances.append(i["balance"])
-    personasconmasdinero=[]
+    personaconmasdinero=[]
     for elem,elem1,elem2 in zip(nombres,apellidos,balances):
         if elem2==max(balances):
-            personaconmasdinero.append(elem,elem1,elem2)
-    return nombres,apellidos,balances, personasconmasdinero
+            personaconmasdinero.append("La persona más adinerada es "+elem+" "+elem1+" con "+elem2+" euros.")
+    return nombres,apellidos,balances,personaconmasdinero
+
+#Ejercicio 4.
+
+def EncontrarAmigos(nombre,datos):
+    nombres=[]
+    apellidos=[]
+    hobbies=[]
+    for i in datos:
+        for elem in i["friends"]:
+            if elem["name"]==nombre:
+                nombres.append(i["name"]["first"])
+                apellidos.append(i["name"]["last"])
+                hobbies.append(i["hobbie"])
+    return nombres, apellidos, hobbies
+
+#Ejercicio 5.
+
+def OpenStreetMap(nombre,apellido,datos):
+    
+
+#Funciones extra.
+
+def ValidarSioNo(siono):
+    if siono=="S" or siono=="N":
+        return True
+
+def ValidarRango(minimo,maximo):
+    if minimo<maximo:
+        return True
--- Principal.py
+++ Principal.py
@@ -36,7 +36,16 @@ while True:
         print("CONTAR USUARIOS ACTIVOS")
         print("-----------------------")
         print()
-        
+        nombres,apellidos=ContarOnline(datos)
+        print(len(nombres))
+        print()
+        siono=input("¿Deseas ver el nombre de dichas personas? (S/N): ")
+        while ValidarSioNo(siono)!=True:
+            siono=input("La respuesta introducida no es válida. Vuelve a intentarlo: ")
+        if siono=="S":
+            print()
+            for elem,elem1 in zip(nombres,apellidos):
+                print(elem, elem1)
         print()
     elif opcion==3:
         print()
@@ -44,7 +53,20 @@ while True:
         print("PERSONAS DENTRO DEL RANGO")
         print("-------------------------")
         print()
-        
+        minimo=float(input("Introduce el valor mínimo del rango a buscar: "))
+        maximo=float(input("Introduce el valor máximo del rango a buscar: "))
+        while ValidarRango(minimo,maximo)!=True:
+            maximo=float(input("El valor máximo introducido es menor o igual que el mínimo. Vuelve a intentarlo: "))
+        print()
+        nombres,apellidos,balances,personaadinerada=BalancePersonas(minimo,maximo,datos)
+        if len(nombres)>0:
+            for elem, elem1, elem2 in zip(nombres,apellidos,balances):
+                print(elem, elem1, "----------", elem2)
+            print()
+            for i in personaadinerada:
+                print(i)
+        else:
+            print("No se han encontrado personas dentro del rango introducido.")
         print()
     elif opcion==4:
         print()
@@ -52,7 +74,14 @@ while True:
         print("AMIGO DE QUIÉN")
         print("--------------")
         print()
-        
+        nombre=input("Introduce el nombre de la persona que deseas buscar: ")
+        print()
+        nombres,apellidos,hobbies=EncontrarAmigos(nombre,datos)
+        if len(nombres)>0:
+            for elem, elem1, elem2 in zip(nombres,apellidos,hobbies):
+                print(nombre, "es amigo de", elem, elem1, "y el hobbie que comparten es", elem2+".")
+        else:
+            print("No se ha encontrado la persona introducida.")
         print()
     elif opcion==5:
         print()
@@ -60,7 +89,12 @@ while True:
         print("OPENSTREETMAP")
         print("-------------")
         print()
-        
+        nombre=input("Introduce el nombre de la persona a buscar: ")
+        apellido=input("Introduce el apellido de la persona a buscar: ")
+        if len(OpenStreetMap(nombre,apellido,datos))>0:
+            print(OpenStreetMap(nombre,apellido,datos))
+        else:
+            print("No se ha encontrado la persona introducida.")
         print()
     elif opcion==6:
         print()
